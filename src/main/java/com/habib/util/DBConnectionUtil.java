085353bd467c8df6fb55be43ced1f4936850e94f
Habibur Rahman <habibur0199mhr@gmail.com>
2023-10-18 00:48:21 +0600

    DBCconnection

---
+++ src/main/java/com/habib/util/DBConnectionUtil.java
@@ -0,0 +1,36 @@
+package com.habib.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectionUtil {
+
+	// Define the database properties
+	private static final String URL = "jdbc:mysql://localhost:3306/employeedirectory";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	private static Connection connection = null;
+	
+	// Define the static method
+	
+	public static Connection openConnection() {
+		// check the connection
+		if (connection != null) {
+			return connection;
+		}
+		// load the driver
+		// get the connection
+		
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// return connection
+		return connection;
+	}
+	
+}
